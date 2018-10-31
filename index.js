3d65d925699f9059ffdacb6b6737392d3670be46
alpha <827130441@qq.com>
2018-10-31 09:18:43 +0800

    project init

---
+++ index.js
@@ -0,0 +1,12 @@
+const Koa = require('koa');
+const static = require('koa-static')
+const path = require('path')
+const app = new Koa();
+
+const staticPath = './public'
+
+app.use(static(
+  path.join( __dirname,  staticPath)
+))
+
+app.listen(3030);
---
+++ public/precache-manifest.b800d08d3337b81a0e7365a4fe516f20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2148f5ece632f98411649345fe42ab8d",
+    "url": "/static/media/avatar.2148f5ec.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ec0799e776fbca1700c6",
+    "url": "/static/js/main.ec0799e7.chunk.js"
+  },
+  {
+    "revision": "de68bf300139386c17ed",
+    "url": "/static/js/1.de68bf30.chunk.js"
+  },
+  {
+    "revision": "ec0799e776fbca1700c6",
+    "url": "/static/css/main.325c1101.chunk.css"
+  },
+  {
+    "revision": "d0c1bf359f12739ec94452f6a1839f69",
+    "url": "/index.html"
+  }
+];
